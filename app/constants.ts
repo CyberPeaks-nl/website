f735072daaa763470d7e747dbc232564c6b3b85d
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2023-07-25 10:20:50 +0200

    Adjust snippet text

--- app/constants.ts
+++ app/constants.ts
@@ -2,4 +2,4 @@ export const COMPANY_NAME = "CyberPeaks";
 export const FULL_NAME = "Teun Zengerink";
 export const TAG_LINE = "Guide towards digital heights";
 export const SNIPPET =
-  "Software engineer with a creative mindset. Passionate about producing output that has an impact on its users and society. Admired by peers for being reliable, authentic, critical, empathic and having an eye for beauty.";
+  "Software engineer with over 13 years of professional experience. Passionate about developing creative solutions that have an impact on its users and society. Motivated to guide organisations in implementing Agile/Scrum practices, mentor junior developers and improve engineering processes. Admired by peers for being reliable, critical, authentic, outspoken, opinionated and emphatic.";
